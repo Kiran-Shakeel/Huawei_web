b6418952ae2eab870cf7415e3363e85cb559433e
Kiran Shakeel <kiranshakeel02@gmail.com>
2022-01-15 23:59:38 +0500

    huawei

---
+++ Script.js
@@ -0,0 +1,99 @@
+function Myfunction(id)
+{
+	
+
+	if(document.getElementById(id)==button1)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+	    document.getElementById("Mobdiv1").style.display="block";
+	}
+	else if(document.getElementById(id)==button2)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv2").style.display="block";
+	}
+	else if(document.getElementById(id)==button3)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv3").style.display="block";
+	}
+	else if(document.getElementById(id)==button4)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv4").style.display="block";
+	}
+	else if(document.getElementById(id)==button6)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv6").style.display="block";
+	}
+	else if(document.getElementById(id)==button7)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv7").style.display="block";
+	}
+	else if(document.getElementById(id)==button8)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv8").style.display="block";
+	}
+	else if(document.getElementById(id)==button9)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv9").style.display="block";
+	}
+	else if(document.getElementById(id)==button10)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv10").style.display="block";
+	}
+	else if(document.getElementById(id)==button11)
+	{
+		document.getElementById("Mobdiv").style.display="none";
+		document.getElementById("Mobdiv11").style.display="block";
+		
+	}
+	
+	else
+	{
+		document.getElementById("Mobdiv1").style.display="none";
+		document.getElementById("Mobdiv2").style.display="none";
+		document.getElementById("Mobdiv3").style.display="none";
+		document.getElementById("Mobdiv4").style.display="none";
+		document.getElementById("Mobdiv6").style.display="none";
+		document.getElementById("Mobdiv7").style.display="none";
+		document.getElementById("Mobdiv8").style.display="none";
+		document.getElementById("Mobdiv9").style.display="none";
+		document.getElementById("Mobdiv10").style.display="none";
+		document.getElementById("Mobdiv11").style.display="none";
+		
+	    document.getElementById("Mobdiv12").style.display="block";
+	}
+}	
+function register(id)
+{
+	document.getElementById("Mobdiv12").style.display="none";
+	document.getElementById("Mobdiv13").style.display="block";
+}
+function sent(id)
+{
+	alert("sent");
+}
+function about()
+{
+	document.getElementById("Mobdiv").style.display="none";
+	document.getElementById("Mobdiv1").style.display="none";
+		document.getElementById("Mobdiv2").style.display="none";
+		document.getElementById("Mobdiv3").style.display="none";
+		document.getElementById("Mobdiv4").style.display="none";
+		document.getElementById("Mobdiv6").style.display="none";
+		document.getElementById("Mobdiv7").style.display="none";
+		document.getElementById("Mobdiv8").style.display="none";
+		document.getElementById("Mobdiv9").style.display="none";
+		document.getElementById("Mobdiv10").style.display="none";
+		document.getElementById("Mobdiv11").style.display="none";
+		document.getElementById("Mobdiv12").style.display="none";
+	document.getElementById("Mobdiv13").style.display="none";
+	document.getElementById("Mobdiv14").style.display="block";
+}	
+
